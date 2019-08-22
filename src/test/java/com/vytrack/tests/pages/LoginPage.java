8bd0c461f625e87f032a67976b0941da16e3a1a6
comolokko13 <47096187+comolokko13@users.noreply.github.com>
2019-08-22 18:34:35 -0500

    Update LoginPage.java
    selam

--- src/test/java/com/vytrack/tests/pages/LoginPage.java
+++ src/test/java/com/vytrack/tests/pages/LoginPage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
-
+//selam
     public LoginPage(){
         PageFactory.initElements(Driver.getDriver(),this);
 
